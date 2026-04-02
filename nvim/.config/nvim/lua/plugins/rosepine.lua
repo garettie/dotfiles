fde8498176e57d41fda38fad7874ac87fe9b2944
garettie <coldsnp@gmail.com>
2026-04-02 15:49:23 +0800

    update noctalia, kitty and nvim config

--- nvim/.config/nvim/lua/plugins/rosepine.lua
+++ nvim/.config/nvim/lua/plugins/rosepine.lua
@@ -4,9 +4,12 @@ return {
   name = "rose-pine",
   config = function()
     require("rose-pine").setup({
-      styles = {
-        transparency = false,
+      highlight_groups = {
+        Normal = { bg = "#17151f" }, -- default Rose Pine base
+        NormalNC = { bg = "#17151f" }, -- inactive windows
+        NormalFloat = { bg = "#17151f" }, -- floating windows
       },
     })
+    vim.cmd("colorscheme rose-pine")
   end,
 }

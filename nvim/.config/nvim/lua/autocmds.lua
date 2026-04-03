4244b1b2c9a6d679b23f13285265e28fc757db65
garettie <coldsnp@gmail.com>
2026-04-03 15:02:24 +0800

    change nvchad indents to 4, add cthulu dashboard, switch to hexa34c colorscheme

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -1,1 +1,21 @@
 require "nvchad.autocmds"
+
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("VimEnter", {
+  callback = function()
+    local socket = vim.env.KITTY_LISTEN_ON
+    if socket then
+      vim.fn.system("kitty @ --to=" .. socket .. " set-spacing padding=0 margin=0")
+    end
+  end,
+})
+
+autocmd("VimLeavePre", {
+  callback = function()
+    local socket = vim.env.KITTY_LISTEN_ON
+    if socket then
+      vim.fn.system("kitty @ --to=" .. socket .. " set-spacing padding=10 margin=10")
+    end
+  end,
+})
--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -5,10 +5,14 @@ require "nvchad.options"
 local o = vim.o
 o.cursorlineopt = "both" -- to enable cursorline!
 o.rnu = true
+-- Indenting
+o.shiftwidth = 4
+o.tabstop = 4
+o.softtabstop = 4
 vim.diagnostic.config {
-  virtual_text = false, -- or true, your call
-  signs = true,
-  underline = true,
-  update_in_insert = false,
-  float = { border = "rounded" },
+    virtual_text = false, -- or true, your call
+    signs = true,
+    underline = true,
+    update_in_insert = false,
+    float = { border = "rounded" },
 }

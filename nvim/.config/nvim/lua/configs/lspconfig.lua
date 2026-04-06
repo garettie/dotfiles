b090834044ba07dcb9d75c6ca1ea2369547a414d
garettie <coldsnp@gmail.com>
2026-04-06 11:16:49 +0800

    add nvim-surround, venv-selector, and which-key groupings

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,7 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "vtsls", "jsonls", "emmet_language_server", "tailwindcss", "eslint-lsp" }
+local servers =
+    { "html", "cssls", "vtsls", "jsonls", "emmet_language_server", "tailwindcss", "eslint-lsp", "basedpyright" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -9,29 +9,31 @@ map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "NvimTree",
-  callback = function()
-    local api = require "nvim-tree.api"
-    local opts = { buffer = true, silent = true }
-    map("n", "l", api.node.open.edit, opts)
-    map("n", "h", api.node.navigate.parent_close, opts)
-  end,
+    pattern = "NvimTree",
+    callback = function()
+        local api = require "nvim-tree.api"
+        local opts = { buffer = true, silent = true }
+        map("n", "l", api.node.open.edit, opts)
+        map("n", "h", api.node.navigate.parent_close, opts)
+    end,
 })
 
 -- Keyboard users
 map("n", "<C-t>", function()
-  require("menu").open "default"
+    require("menu").open "default"
 end, {})
 
 -- mouse users + nvimtree users!
 map({ "n", "v" }, "<RightMouse>", function()
-  require("menu.utils").delete_old_menus()
+    require("menu.utils").delete_old_menus()
 
-  vim.cmd.exec '"normal! \\<RightMouse>"'
+    vim.cmd.exec '"normal! \\<RightMouse>"'
 
-  -- clicked buf
-  local buf = vim.api.nvim_win_get_buf(vim.fn.getmousepos().winid)
-  local options = vim.bo[buf].ft == "NvimTree" and "nvimtree" or "default"
+    -- clicked buf
+    local buf = vim.api.nvim_win_get_buf(vim.fn.getmousepos().winid)
+    local options = vim.bo[buf].ft == "NvimTree" and "nvimtree" or "default"
 
-  require("menu").open(options, { mouse = true })
+    require("menu").open(options, { mouse = true })
 end, {})
+
+map("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })

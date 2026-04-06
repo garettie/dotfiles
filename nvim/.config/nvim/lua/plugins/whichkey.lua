116b37c5dfa7fc4257c95a9eb580a5dbbc83f391
garettie <coldsnp@gmail.com>
2026-04-06 11:17:37 +0800

    add nvim-surround, venv-selector, and which-key groupings

---
+++ nvim/.config/nvim/lua/plugins/whichkey.lua
@@ -0,0 +1,16 @@
+return {
+    "folke/which-key.nvim",
+    opts = {
+        spec = {
+            { "<leader>c", group = "cheatsheet/commits" },
+            { "<leader>d", group = "diagnostics" },
+            { "<leader>f", group = "find" },
+            { "<leader>g", group = "git" },
+            { "<leader>m", group = "marks" },
+            { "<leader>p", group = "term picker" },
+            { "<leader>r", group = "relative numbers" },
+            { "<leader>t", group = "themes" },
+            { "<leader>w", group = "which-key" },
+        },
+    },
+}

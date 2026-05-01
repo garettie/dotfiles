21dc0bdf62f4bf51a78bb19c3dd6c56b36d8b7da
garettie <coldsnp@gmail.com>
2026-05-01 10:59:04 +0800

    rebase

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -3,19 +3,28 @@ require "nvchad.autocmds"
 local autocmd = vim.api.nvim_create_autocmd
 
 autocmd("VimEnter", {
-  callback = function()
-    local socket = vim.env.KITTY_LISTEN_ON
-    if socket then
-      vim.fn.system("kitty @ --to=" .. socket .. " set-spacing padding=0 margin=0")
-    end
-  end,
+    callback = function()
+        local socket = vim.env.KITTY_LISTEN_ON
+        if socket then
+            vim.fn.system("kitty @ --to=" .. socket .. " set-spacing padding=0 margin=0")
+        end
+    end,
 })
 
 autocmd("VimLeavePre", {
-  callback = function()
-    local socket = vim.env.KITTY_LISTEN_ON
-    if socket then
-      vim.fn.system("kitty @ --to=" .. socket .. " set-spacing padding=10 margin=10")
-    end
-  end,
+    callback = function()
+        local socket = vim.env.KITTY_LISTEN_ON
+        if socket then
+            vim.fn.system("kitty @ --to=" .. socket .. " set-spacing padding=10 margin=10")
+        end
+    end,
+})
+
+autocmd("BufDelete", {
+    callback = function()
+        local bufs = vim.t.bufs
+        if #bufs == 1 and vim.api.nvim_buf_get_name(bufs[1]) == "" then
+            vim.cmd "Nvdash"
+        end
+    end,
 })
--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -1,7 +1,25 @@
 require("nvchad.configs.lspconfig").defaults()
 
+<<<<<<< HEAD
 local servers =
     { "html", "cssls", "vtsls", "jsonls", "emmet_language_server", "tailwindcss", "eslint-lsp", "basedpyright" }
 vim.lsp.enable(servers)
+=======
+local servers = {
+    "html",
+    "cssls",
+    "emmet_language_server",
+    "tsgo",
+    "eslint",
+    "jsonls",
+    "tailwindcss",
+    "taplo",
+    "basedpyright",
+    "bashls",
+    "lua_ls",
+    "marksman",
+    "rust_analyzer",
+}
+>>>>>>> e25ec50 (noctalia update)
 
--- read :h vim.lsp.config for changing options of lsp servers
+vim.lsp.enable(servers)
